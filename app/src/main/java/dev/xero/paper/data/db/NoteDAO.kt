b242e3bf95b37ba869ec940c85ed1f905f7118c4
Xero kun <elo48858@gmail.com>
2023-03-08 21:05:34 +0100

    `✨UPDATE` Setup NoteDAO

--- app/src/main/java/dev/xero/paper/data/db/NoteDAO.kt
+++ app/src/main/java/dev/xero/paper/data/db/NoteDAO.kt
@@ -1,2 +1,20 @@
 package dev.xero.paper.data.db
 
+import androidx.room.*
+import dev.xero.paper.data.db.entities.Note
+
+@Dao
+interface NoteDAO {
+
+	@Query("SELECT * FROM notes")
+	fun getNotes(): List<Note>
+
+	@Query("SELECT * FROM notes WHERE id =:id")
+	suspend fun getNote(id: Int): Note?
+
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	suspend fun addNote(note: Note)
+
+	@Delete
+	suspend fun deleteNote(note: Note)
+}
