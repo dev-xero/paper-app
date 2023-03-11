514a4395fcc3190b7d40b8353ef812f00a6f9400
Xero kun <elo48858@gmail.com>
2023-03-11 16:02:33 +0100

    `✨UPDATE` Added Home Screen Text

--- app/src/main/java/dev/xero/paper/presentation/notes/noteslist/HomeScreen.kt
+++ app/src/main/java/dev/xero/paper/presentation/notes/noteslist/HomeScreen.kt
@@ -19,14 +19,21 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import dev.xero.paper.R
 import dev.xero.paper.presentation.notes.noteslist.components.SearchBar
+import dev.xero.paper.presentation.ui.theme.Black
+import dev.xero.paper.presentation.ui.theme.Grey500
+import dev.xero.paper.presentation.ui.theme.OnSurface
+import dev.xero.paper.presentation.ui.theme.Primary
 
 @Composable
 fun HomeScreen(
@@ -50,8 +57,34 @@ fun HomeScreen(
 	)
 	{ padding ->
 		LazyColumn(
-			modifier = modifier.padding(8.dp)
+			modifier = modifier.padding(12.dp)
 		) {
+			item {
+				Text(
+					text = stringResource(id = R.string.home_text),
+					modifier = Modifier.padding(
+						top = 12.dp,
+						bottom = 8.dp
+					),
+					style = MaterialTheme.typography.h3,
+					color = when(isDarkTheme) {
+						true -> Black
+						else -> OnSurface
+					}
+				)
+				Text(
+					text = stringResource(id = R.string.efficiently),
+					modifier = Modifier.padding(
+						bottom = 12.dp
+					),
+					style = MaterialTheme.typography.h4,
+					color = when(isDarkTheme) {
+						true -> Primary
+						else -> Grey500
+					}
+				)
+			}
+
 			items(notes.value, key = { note -> note.id }) { note ->
 				Text(text = note.title)
 			}
--- app/src/main/java/dev/xero/paper/presentation/ui/theme/Type.kt
+++ app/src/main/java/dev/xero/paper/presentation/ui/theme/Type.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2023 - Xero
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.xero.paper.presentation.ui.theme
 
 import androidx.compose.material.Typography
@@ -54,7 +69,13 @@ val Typography = Typography(
 	h3 = TextStyle(
 		fontFamily = CoreSans,
 		fontWeight = FontWeight.Bold,
-		fontSize = 24.sp
+		fontSize = 22.sp
+	),
+
+	h4 = TextStyle(
+		fontFamily = CoreSans,
+		fontWeight = FontWeight.Normal,
+		fontSize = 20.sp
 	),
 
 	subtitle1 = TextStyle(
