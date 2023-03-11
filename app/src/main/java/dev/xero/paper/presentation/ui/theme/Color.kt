cbd6e6b8de8e5e616d7095483b5c372e8e90d551
Xero kun <elo48858@gmail.com>
2023-03-11 12:21:25 +0100

    `✨UPDATE` Added App Color Palette

--- app/src/main/java/dev/xero/paper/presentation/ui/theme/Color.kt
+++ app/src/main/java/dev/xero/paper/presentation/ui/theme/Color.kt
@@ -1,8 +1,29 @@
+/*
+ * Copyright (C) 2023 - Xero
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.xero.paper.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Primary = Color(0xFF5DE07A)
+val OnSurface = Color(0xFFEBF7EE)
+val Secondary = Color(0xFFCAF0D2)
+val Grey100 = Color(0xFF8A978D)
+val Grey300 = Color(0xFF66776A)
+val Grey500 = Color(0xFF445347)
+val Black = Color(0xFF070809)
+val BgDark = Color(0xFF080B0D)
+val SurfaceDark = Color(0xFF0C1013)
+
