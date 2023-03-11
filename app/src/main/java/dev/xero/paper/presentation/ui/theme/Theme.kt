c672c6d50d1fadad77675e9560b82f9f0159a154
Xero kun <elo48858@gmail.com>
2023-03-11 12:25:35 +0100

    `✨UPDATE` Updated Theme Colors

--- app/src/main/java/dev/xero/paper/presentation/ui/theme/Theme.kt
+++ app/src/main/java/dev/xero/paper/presentation/ui/theme/Theme.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2023 - Xero
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.xero.paper.presentation.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -7,24 +22,17 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-	primary = Purple200,
-	primaryVariant = Purple700,
-	secondary = Teal200
+	primary = Primary,
+	surface = SurfaceDark,
+	background = BgDark,
+	onSurface = Grey100
 )
 
 private val LightColorPalette = lightColors(
-	primary = Purple500,
-	primaryVariant = Purple700,
-	secondary = Teal200
-
-	/* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+	primary = Primary,
+	surface = Secondary,
+	background = OnSurface,
+	onSurface = Grey500
 )
 
 @Composable
