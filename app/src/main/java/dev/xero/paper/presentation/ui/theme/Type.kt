4c8ea4095e86589818d5cbff2902e02e221dd266
Xero kun <elo48858@gmail.com>
2023-03-11 15:36:21 +0100

    `✨UPDATE` Defined App Typography

--- app/src/main/java/dev/xero/paper/presentation/ui/theme/Type.kt
+++ app/src/main/java/dev/xero/paper/presentation/ui/theme/Type.kt
@@ -2,27 +2,76 @@ package dev.xero.paper.presentation.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.xero.paper.R
 
-// Set of Material typography styles to start with
+val AvenirLT = FontFamily(
+	Font(
+		resId = R.font.avenir_lt_book,
+		weight = FontWeight.Normal
+	),
+
+	Font(
+		resId = R.font.avenir_lt_medium,
+		weight = FontWeight.Medium
+	),
+
+	Font(
+		resId = R.font.avenir_lt_black,
+		weight = FontWeight.Black
+	)
+)
+
+val CoreSans = FontFamily(
+	Font(
+		resId = R.font.core_sans_regular,
+		weight = FontWeight.Normal
+	),
+
+	Font(
+		resId = R.font.core_sans_bold,
+		weight = FontWeight.Bold
+	)
+)
+
+val OutFit = FontFamily(
+	Font(
+		resId = R.font.outfit_regular,
+		weight = FontWeight.Normal
+	)
+)
+
 val Typography = Typography(
-	body1 = TextStyle(
-		fontFamily = FontFamily.Default,
+	caption = TextStyle(
+		fontFamily = CoreSans,
 		fontWeight = FontWeight.Normal,
 		fontSize = 16.sp
-	)
-	/* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+	),
+
+	h3 = TextStyle(
+		fontFamily = CoreSans,
+		fontWeight = FontWeight.Bold,
+		fontSize = 24.sp
+	),
+
+	subtitle1 = TextStyle(
+		fontFamily = AvenirLT,
+		fontWeight = FontWeight.Medium,
+		fontSize = 16.sp
+	),
+
+	subtitle2 = TextStyle(
+		fontFamily = CoreSans,
+		fontWeight = FontWeight.Bold,
+		fontSize = 14.sp
+	),
+
+	body1 = TextStyle(
+		fontFamily = OutFit,
+		fontWeight = FontWeight.Normal,
+		fontSize = 16.sp
+	),
 )
