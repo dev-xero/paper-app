4d654d0d227f7efc6d3d5d9fd3afb9aed5ac1b61
Xero kun <elo48858@gmail.com>
2023-03-13 18:59:10 +0100

    `✨UPDATE` Moved App to app Package

--- app/src/main/java/dev/xero/paper/app/PaperApplication.kt
+++ app/src/main/java/dev/xero/paper/app/PaperApplication.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.xero.paper
+package dev.xero.paper.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
