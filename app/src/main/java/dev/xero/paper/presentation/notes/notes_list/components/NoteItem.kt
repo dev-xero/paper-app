343054035d938ef4711f0ec7737f8c5efd620575
Xero kun <elo48858@gmail.com>
2023-03-14 20:42:46 +0100

    `✨UPDATE` Created NoteItem Composable

---
+++ app/src/main/java/dev/xero/paper/presentation/notes/notes_list/components/NoteItem.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2023 - Xero
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.xero.paper.presentation.notes.notes_list.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.xero.paper.domain.model.NoteDBEntity
+import dev.xero.paper.presentation.ui.theme.Grey500
+import dev.xero.paper.presentation.ui.theme.Primary
+import dev.xero.paper.presentation.ui.theme.Secondary
+import dev.xero.paper.presentation.ui.theme.SurfaceDark
+
+@Composable
+fun NoteItem(
+	note: NoteDBEntity,
+	isDarkTheme: Boolean,
+	modifier: Modifier = Modifier
+) {
+	Card(
+		modifier = modifier.padding(8.dp),
+		shape = RoundedCornerShape(4.dp),
+		elevation = 0.dp,
+		backgroundColor = if (isDarkTheme) SurfaceDark else Secondary
+	) {
+		Column() {
+			Text(
+				text = note.title,
+				style = MaterialTheme.typography.subtitle1,
+				fontSize = 18.sp,
+				color = if (isDarkTheme) Primary else Grey500
+			)
+		}
+	}
+}
